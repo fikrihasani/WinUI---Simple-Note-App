f4d3181dccd9f3e35ffb7a2071f5f427459eb729
fikrihasani <mfikrihasani@gmail.com>
2025-02-23 11:11:42 +0700

    add navigation to detail page. Next: edit the note detail

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Windowing;
 using WinRT.Interop;
 using Microsoft.UI;
+using System.Collections.ObjectModel;
+using App1.Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,23 +28,54 @@ namespace App1
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public ObservableCollection<Note> Notes { get; } = new ObservableCollection<Note>();
         public MainWindow()
         {
             this.InitializeComponent();
             this.setWindowSize(800, 600);
+            textList.ItemsSource = Notes;
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            displayText.Text = "You type: "+textInput.Text;
+            addTextToList();
         }
 
+        private void addTextToList()
+        {
+            if (!String.IsNullOrEmpty(textInput.Text)) {
+                Notes.Add(
+                    new Note
+                        { 
+                            title = textInput.Text, 
+                            contents = string.Empty
+                    }
+                  );
+                textInput.Text = "";
+            }
+        }
         private void setWindowSize(int width, int height)
         {
             var hwnd = WindowNative.GetWindowHandle(this);
             var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
             var appwindow = AppWindow.GetFromWindowId(windowId);
             appwindow.Resize(new Windows.Graphics.SizeInt32(width, height));
+        }
+
+
+        private void textList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            if (e.ClickedItem !=null)
+            {
+                //do some type casting
+                Note clickedNote = (Note)e.ClickedItem;
+                // Create a new instance of BlankPage1 and assign the selected Note
+                BlankPage1 detailPage = new BlankPage1();
+                detailPage.selectedNote = clickedNote;
+
+                // Replace the entire window content with the new page
+                this.Content = detailPage;
+            }
         }
     }
 }
---
+++ Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Models
+{
+    public class Note
+    {
+        public String title { get; set; } = string.Empty;
+        public String contents { get; set; } = string.Empty; 
+    }
+}
